5af653648f2ab47e997fee957dda77f7748a63dc
felipe reusch <felipereusch@MacBook-Air-de-felipe.local>
2021-11-23 12:21:22 -0300

    add class hello

---
+++ src/main/java/com/example/jenkinstum/controller/Hello.java
@@ -0,0 +1,13 @@
+package com.example.jenkinstum.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+
+    @GetMapping("/hello")
+    public String message(){
+        return "Hello Word";
+    }
+}
