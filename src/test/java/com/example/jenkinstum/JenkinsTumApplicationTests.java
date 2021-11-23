f1b53167b93a823247dc1026f8859ce691e19467
felipe reusch <felipereusch@MacBook-Air-de-felipe.local>
2021-11-23 11:12:21 -0300

    jenkins

---
+++ src/test/java/com/example/jenkinstum/JenkinsTumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkinstum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsTumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
